fa3304529ddfdbb633f02c64e1041890e92dc993
Luca Marchi <luca1010.marchi@gmail.com>
2026-02-22 16:03:52 +0100

    update person

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import utils.Logger;
 
 public class Main {
     public static void main(final String[] args){
-        Person person = new Person("Gianluca", 29);
+        Person person = new Person("Luca", 22);
         Logger.logPrintString("Hello " + person);
     }
 }
